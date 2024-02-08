3dd063514c6d703a99587f1e5bc0d19d30ecba19
camrodriguez48 <144289636+camrodriguez48@users.noreply.github.com>
2024-02-07 20:33:04 -0500

    test
    subir proyecto

---
+++ rodriguez/src/main/java/com/mycompany/rodriguez/Rodriguez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rodriguez;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Rodriguez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
